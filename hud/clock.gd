b1df128d741b378392f2961af8afa2411457e10b
Julian Holfeld <julian@uni-kassel.de>
2021-10-16 11:32:49 +0200

    feat: add clock

---
+++ hud/clock.gd
@@ -0,0 +1,30 @@
+extends Control
+
+var seconds: int = 0
+var minutes: int = 0
+var hours: int = 0
+
+onready var small_pointer = $background/small
+onready var big_pointer = $background/big
+
+
+func _on_time_timeout():
+	_calculate_time()
+	_move_pointer()
+
+
+func _move_pointer():
+	big_pointer.rotation_degrees = minutes * 6
+	small_pointer.rotation_degrees = 30 * hours
+	pass
+
+
+func _calculate_time():
+	minutes += 1
+	print(minutes)
+	
+	if minutes == 60:
+		minutes = 0
+		hours += 1
+	if hours == 12:
+		hours = 0
