aa95454f8e6a750b51283d49ea05dd789013f855
Julian Holfeld <julian@uni-kassel.de>
2021-10-16 14:20:57 +0200

    feat: open up quest-journal

---
+++ hud/hud.gd
@@ -0,0 +1,21 @@
+extends Node
+
+onready var clock = $clock
+var journal = preload("res://menus/journal.tscn")
+
+func _process(delta):
+	_handle_input()
+
+
+func _handle_input():
+	if Input.is_action_just_pressed("journal"):
+		get_tree().paused = true
+		add_child(journal.instance())
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().paused = false
+		for n in get_children():
+			if n == clock: 
+				continue
+			remove_child(n)
+			n.queue_free()
