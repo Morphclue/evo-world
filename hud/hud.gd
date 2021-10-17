776a9c0a44c02164441f99d0a8b9101ed042e204
Julian Holfeld <julian@uni-kassel.de>
2021-10-17 11:30:54 +0200

    refactor: journal window handling

--- hud/hud.gd
+++ hud/hud.gd
@@ -2,7 +2,7 @@ extends Node
 
 onready var clock: Control = $clock
 var journal: PackedScene  = preload("res://menus/journal.tscn")
-
+var open_journal: bool = false
 
 func _process(_delta):
 	_handle_input()
@@ -10,9 +10,23 @@ func _process(_delta):
 
 func _handle_input():
 	if Input.is_action_just_pressed("journal"):
-		get_tree().paused = true
-		add_child(journal.instance())
+		if open_journal:
+			_close_windows()
+		else:
+			_add_window(journal)
 	
 	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().paused = false
-		Utils.remove_children(self, [clock])
+		_close_windows()
+
+
+func _add_window(scene: PackedScene):
+	open_journal = true
+	get_tree().paused = true
+	add_child(journal.instance())
+
+
+func _close_windows():
+	open_journal = false
+	get_tree().paused = false
+	Utils.remove_children(self, [clock])
+	
