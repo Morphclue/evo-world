c301cc62daa63e63add40cb0d32f4d9636f62f8e
Julian Holfeld <julian@uni-kassel.de>
2021-10-17 12:41:15 +0200

    feat: add basic journal UI

--- menus/journal.gd
+++ menus/journal.gd
@@ -1,11 +1,18 @@
 extends Control
 
 var quests: Array = []
+onready var item_list: ItemList = $vBox/itemList
+onready var description: Label = $vBox/hBox/panelContainer/description
+onready var back: Button = $vBox/hBox/back
 
 func _ready():
 	_load_quests()
-	pass
+	_load_ui()
 
 
 func _load_quests():
 	quests = PlayerVariables.quest_list
+
+
+func _load_ui():
+	pass
