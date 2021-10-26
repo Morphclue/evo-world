ee4b1825447f4547ca5c0dc1c01fd89200b7719b
Julian Holfeld <julian@uni-kassel.de>
2021-10-26 09:49:31 +0200

    fix: remove unused back button

--- ui/menus/journal.gd
+++ ui/menus/journal.gd
@@ -3,7 +3,6 @@ extends Control
 var quests: Array = []
 onready var item_list: ItemList = $vBox/itemList
 onready var description: Label = $vBox/hBox/panelContainer/description
-onready var back: Button = $vBox/hBox/back
 
 func _ready() -> void:
 	_load_quests()
