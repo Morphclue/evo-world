3718341c7110cd9205da92b1369930932f98e5c1
Julian Holfeld <julian@uni-kassel.de>
2021-11-02 14:52:02 +0100

    feat: add knockback

--- combat/combatEntity.gd
+++ combat/combatEntity.gd
@@ -1,31 +1,48 @@
 extends KinematicBody2D
 
 export (NodePath) var target
-onready var collider: CollisionShape2D = $collider
 
 var velocity: Vector2
 var knockback: Vector2
 var enemy: KinematicBody2D
+var enemies_colliding: bool = false
 
 func _ready() -> void:
 	enemy = get_node(target)
 
 
 func _physics_process(delta: float) -> void:
-	_move_to_target(delta)
 	_calculate_knockback(delta)
-	velocity = move_and_slide(velocity)
+	_move_to_target(delta)
 
 
 func _move_to_target(delta: float)  -> void:
-	var direction: Vector2 = (enemy.position - position).normalized()
+	var direction: Vector2 = (enemy.global_position - global_position).normalized()
 	
 	velocity = velocity.move_toward(
-		direction * (Constants.MAX_SPEED), 
+		direction * (Constants.MAX_SPEED / 3.0), 
 		delta * Constants.ACCELERATION
 	)
+	velocity = move_and_slide(velocity)
 
 
-func _calculate_knockback(_delta: float)  -> void:
+func _calculate_knockback(delta: float)  -> void:
+	knockback = knockback.move_toward(
+		Vector2.ZERO, 
+		delta * Constants.ACCELERATION)
 	
-	pass
+	knockback = move_and_slide(knockback)
+
+
+func _add_knockback():
+	knockback = Vector2(rand_range(-1, 1), rand_range(-1, 1))
+	velocity += knockback * Constants.KNOCKBACK_MULTIPLICATOR
+
+
+func _on_area2D_area_entered(_area: Area2D) -> void:
+	enemies_colliding = true
+	_add_knockback()
+
+
+func _on_area2D_area_exited(_area: Area2D) -> void:
+	enemies_colliding = false
