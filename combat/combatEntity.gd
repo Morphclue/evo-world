0fbf80b3aef0e597a354c13f9b0b53cbf44f2e8b
Julian Holfeld <julian@uni-kassel.de>
2021-11-02 12:49:49 +0100

    feat: move to enemy target

--- combat/combatEntity.gd
+++ combat/combatEntity.gd
@@ -1,18 +1,30 @@
 extends KinematicBody2D
 
 export (NodePath) var target
+onready var collider: CollisionShape2D = $collider
 
-var velocity = Vector2.ZERO
-var knockback = Vector2.ZERO
+var velocity: Vector2
+var knockback: Vector2
+var enemy: KinematicBody2D
+
+func _ready() -> void:
+	enemy = get_node(target)
+
 
 func _physics_process(delta: float) -> void:
 	_move_to_target(delta)
 	_calculate_knockback(delta)
+	velocity = move_and_slide(velocity)
 
 
 func _move_to_target(delta: float)  -> void:
-	pass
+	var direction: Vector2 = (enemy.position - position).normalized()
+	
+	velocity = velocity.move_toward(
+		direction * (Constants.MAX_SPEED), 
+		delta * Constants.ACCELERATION
+	)
 
 
-func _calculate_knockback(delta: float)  -> void:
+func _calculate_knockback(_delta: float)  -> void:
 	pass
